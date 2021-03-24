07125694ca1d690d3ddd00e9bf09dbbfc2022a0e
Paweł Kołodziejczyk <p.kolodziej92@gmail.com>
2021-03-24 18:07:20 +0100

    289. Implementing Error and Success Messages

--- src/js/controller.js
+++ src/js/controller.js
@@ -22,7 +22,7 @@ const controlRecipes = async function () {
     //2. Rendering
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    recipeView.renderError();
   }
 };
 
