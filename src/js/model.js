5325a20746fe6dcf1c5afb011cff2b6efadd69e5
Paweł Kołodziejczyk <p.kolodziej92@gmail.com>
2021-03-24 17:40:16 +0100

    287. Helpers and Configuration Files

--- src/js/model.js
+++ src/js/model.js
@@ -1,16 +1,13 @@
+import { API_URL } from './config.js';
+import { getJSON } from './helpers';
+
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const response = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-
-    const data = await response.json();
-
-    if (!response.ok) throw new Error(`${data.message} (${response.status})`);
+    const data = await getJSON(`${API_URL}/${id}`);
 
     const { recipe } = data.data;
     console.log(recipe);
@@ -26,7 +23,6 @@ export const loadRecipe = async function (id) {
       ingredients: recipe.ingredients,
     };
   } catch (err) {
-    console.error(err);
-    alert(err);
+    console.error(`${err} 😪😪😪😪😪😪😪`);
   }
 };
